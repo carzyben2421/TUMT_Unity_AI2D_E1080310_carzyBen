c18b00eb3f50d5643d38def82f9a6aa054fedc74
carzyben2421 <55227126+carzyben2421@users.noreply.github.com>
2020-01-08 17:08:56 +0800

    010802

---
+++ Unity_UI_E1080310_carzyBen/Assets/Game/GameManager.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+   public AudioMixer mixer;
+
+    public Text loadingText;
+    public Slider loading;
+
+    public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+    public void SetVSFX(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+    public void Play()
+    {
+      //eneManager.LoadScene("AK47");
+        StartCoroutine(Loading());
+    }
+
+    private IEnumerator Loading()
+    {
+        //print("測試1");
+        //yield return new WaitForSeconds(1);
+        //print("測試2");
+
+        AsyncOperation ao = SceneManager.LoadSceneAsync("遊戲場景");
+        ao.allowSceneActivation = false;
+
+        while (ao.isDone == false)
+        {
+            loadingText.text = ((ao.progress / 0.9f) * 100).ToString();
+            loading.value = ao.progress / 0.9f;
+            yield return new WaitForSeconds(0.0001f);
+
+            if (ao.progress == 0.9f && Input.anyKey)
+            {
+                ao.allowSceneActivation = true;
+            }
+
+        }
+
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("0209");
+     }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Unity_UI_E1080310_carzyBen/Assets/Game/Player.cs
@@ -0,0 +1,101 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public int hp = 100;
+    public Slider hpslider;
+
+    public Text textTacos;
+    public int TacosCount;
+    public int TacosTotal;
+
+    public Text textTime;
+    public float gameTime;
+
+    public GameObject final;
+    public Text textBest;
+    public Text textCurrent;
+
+    private void OnTriggerEnter(Collider other)
+    {
+       if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpslider.value = hp;
+            if (hp <= 0) Dead();
+        }
+
+        if (other.tag == "Tacos")
+        {
+            TacosCount++;
+            textTacos.text = "Tacos : " + TacosCount + " / " + TacosTotal;
+            Destroy(other.gameObject);
+        }
+
+        if (other.name =="終點" && TacosCount == TacosTotal)
+        {
+           GameOver();
+        }
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpslider.value = hp;
+            if (hp <= 0) Dead();
+
+        }
+    }
+
+    private void Start()
+    {
+        if (PlayerPrefs.GetFloat("最佳紀錄") == 0)
+        {
+            PlayerPrefs.SetFloat("最佳紀錄", 99999);
+        }
+        TacosTotal = GameObject.FindGameObjectsWithTag("Tacos").Length;
+        textTacos.text = "Tacos : 0 / " + TacosTotal;
+    }
+
+    private void Update()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("F2");
+    }
+
+    private void Dead()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+        textBest.text = "BEST : " + PlayerPrefs.GetFloat("最佳紀錄").ToString("F2");
+        Cursor.lockState = CursorLockMode.None;
+
+        //<FPSControllerLPFP.FpsControllerLPFP>().enabled = false;
+        enabled = false;
+    }
+
+    private void GameOver()
+    {
+        final.SetActive(true);
+        textCurrent.text = "TIME : " + gameTime.ToString("F2");
+
+        if(gameTime < PlayerPrefs.GetFloat("最佳紀錄"))
+        {
+            PlayerPrefs.SetFloat("最佳紀錄", gameTime);
+        }
+
+        textBest.text = "BEST : " + PlayerPrefs.GetFloat("最佳紀錄").ToString("F2");
+
+        Cursor.lockState = CursorLockMode.None;
+
+    }
+}
